dd4022be567f51154d5ebdeb1f7217dd13b7fb9b
Havock48 <adit.github@gmail.com>
2024-04-06 16:16:48 +0200

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+    POPUP
+  </div> */}
+  return (
+    <section className="w-full h-screen relative">
+      
+    </section>
+  )
+}
+
+export default Home
